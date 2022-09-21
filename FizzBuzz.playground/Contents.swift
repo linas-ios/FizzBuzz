0bddc6bc86e766090af3da09941eff9bc5030bbf
Linas <linasn404@gmail.com>
2022-09-21 09:23:03 +0200

    Change new code

---
+++ FizzBuzz.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+let numbers = 1...100
+
+for number in numbers {
+    if number.isMultiple(of: 3) && number.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if number.isMultiple(of: 3) {
+        print("Fizz")
+    } else if number.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(number)
+    }
+}
